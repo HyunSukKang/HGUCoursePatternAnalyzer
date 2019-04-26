d4b567215c17f58a6c10e3bf4d14ce25b78f6d51
HyunSukKang <21400018@handong.edu>
2019-04-26 12:20:55 +0900

    hw4

--- src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
+++ src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
@@ -1,28 +1,28 @@
 package edu.handong.analysis;
 
-import edu.handong.analysis.datamodel.Course;
+//import edu.handong.analysis.datamodel.Course;
 import edu.handong.analysis.datamodel.Student;
 
 public class HGUCoursePatternAnalyzer {
 	
-	String[] lines = {	"1999-1, JC Nam, Java Programming",
-						"1999-2, JC Nam, Programming Language Theory",
-						"1999-1, JC Nam, Data Structures",
-						"2001-1, JC Nam, Database Systems",
-						"2018-1, SB Lim, Java Programming",
-						"2018-2, SB Lim, Programming Language Theory",
-						"2019-1, SB Lim, Data Structures",
-						"2019-1, SB Lim, Algorithm Analysis",
-						"2018-1, SJ Kim, Java Programming",
-						"2018-2, SJ Kim, Programming Language Theory",
-						"2019-1, SJ Kim, Logic Design",
-						"2019-1, SJ Kim, Algorithm Analysis",
-						};
+	private String[] lines = {	"1999-1, JC Nam, Java Programming",
+								"1999-2, JC Nam, Programming Language Theory",
+								"1999-1, JC Nam, Data Structures",
+								"2001-1, JC Nam, Database Systems",
+								"2018-1, SB Lim, Java Programming",
+								"2018-2, SB Lim, Programming Language Theory",
+								"2019-1, SB Lim, Data Structures",
+								"2019-1, SB Lim, Algorithm Analysis",
+								"2018-1, SJ Kim, Java Programming",
+								"2018-2, SJ Kim, Programming Language Theory",
+								"2019-1, SJ Kim, Logic Design",
+								"2019-1, SJ Kim, Algorithm Analysis",
+							};
 
-	int numOfStudents;
-	int numOfCourses;
-	Student[] students;
-	Course[] courses;
+	private int numOfStudents;
+//	private int numOfCourses;
+	private Student[] students;
+//	private Course[] courses;
 	
 	/**
 	 * This method runs our analysis logic to get the list of student and course names from lines.
@@ -31,7 +31,7 @@ public class HGUCoursePatternAnalyzer {
 	public void run(String[] args) {
 		
 		numOfStudents = Integer.parseInt(args[0]);
-		numOfCourses = Integer.parseInt(args[1]);
+//		numOfCourses = Integer.parseInt(args[1]);
 	
 		students = initiateStudentArrayFromLines(lines);
 		
@@ -40,11 +40,11 @@ public class HGUCoursePatternAnalyzer {
 			System.out.println(student.getName());
 		}
 		
-		courses = initiateCourseArrayFromLines(lines);
-		System.out.println("Number of All Courses: " + numOfCourses);
-		for(Course course: courses) {
-			System.out.println(course.getCourseName());
-		}
+//		courses = initiateCourseArrayFromLines(lines);
+//		System.out.println("Number of All Courses: " + numOfCourses);
+//		for(Course course: courses) {
+//			System.out.println(course.getCourseName());
+//		}
 		
 	}
 
@@ -54,37 +54,61 @@ public class HGUCoursePatternAnalyzer {
 	 * @return
 	 */
 	private Student[] initiateStudentArrayFromLines(String[] lines) {
-		
-		// TODO: implement this method
-		
+		String[] tempString;
+		int a = lines.length;
+		int count = 0;
+		int flag = 0;
+		for(int i=0; i<a; i++) {
+			tempString = lines[i].trim().split(", ");
+			if(i == 0) {
+				students[i] = new Student(tempString[1]);
+				count++;
+			}
+			else {
+				for(int j=0; j<count; j++) {
+					if(tempString[1].equals(students[j].getName())) {
+						flag = 1;
+						break;
+					}
+				}
+				
+				if(flag == 1) {
+					flag = 0;
+				}
+				else {
+					students[count] = new Student(tempString[1]);
+					count++;
+				}
+			}
+		}
 		
 		return null;
 	}
 
 	/**
-	 * This method check if there is the same name of the second arugement in the array, students
+	 * This method check if there is the same name of the second argument in the array, students
 	 * @param students
 	 * @param student
 	 * @return boolean
 	 */
-	private boolean studentExist(Student[] students, Student student) {
-		
-		// TODO: implement this method
-
-		return false;
-	}
+//	private boolean studentExist(Student[] students, Student student) {
+//		
+//		// TODO: implement this method
+//
+//		return false;
+//	}
 	
 	/**
 	 * This method returns a Course array to initiate the field, courses, from lines.
 	 * @param lines
 	 * @return
 	 */
-	private Course[] initiateCourseArrayFromLines(String[] lines) {
-		
-		// TODO: implement this method
-		
-		return null;
-	}
+//	private Course[] initiateCourseArrayFromLines(String[] lines) {
+//		
+//		// TODO: implement this method
+//		
+//		return null;
+//	}
 
 	/**
 	 * This method check if there is the same name of the second argument in the array, courses.
@@ -92,11 +116,11 @@ public class HGUCoursePatternAnalyzer {
 	 * @param course
 	 * @return boolean
 	 */
-	private boolean courseExist(Course[] courses, Course course) {
-		
-		// TODO: implement this method
-
-		return false;
-	}
+//	private boolean courseExist(Course[] courses, Course course) {
+//		
+//		// TODO: implement this method
+//
+//		return false;
+//	}
 
 }
